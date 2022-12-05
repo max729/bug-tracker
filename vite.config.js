c3edd772b94f9e5c99f14141097ece84e216e41a
max <max.schoppe@gmx.net>
2022-12-05 16:02:41 +0100

    add vite

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,15 @@ import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: path.resolve(__dirname, 'src/main/resources/templates'),
+  root: path.resolve(__dirname, 'src/main/frontend'),
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      '@' : path.resolve(__dirname, 'src/main/resources')
+      '@' : path.resolve(__dirname, 'src/main/frontend')
     }
   },
   build: {
-    outDir: path.resolve(__dirname, 'target/classes/templates'),
-        manifest: true
+    outDir: path.resolve(__dirname, 'src/main/resources/static')
   },
   server: {
     port: 8081,
