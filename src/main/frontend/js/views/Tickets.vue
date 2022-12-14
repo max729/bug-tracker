af64b75a8ed5e5366cbb641ee8dd82cde4b7dbc7
max <max.schoppe@gmx.net>
2022-12-14 16:22:46 +0100

    add search tickets side

--- src/main/frontend/js/views/Tickets.vue
+++ src/main/frontend/js/views/Tickets.vue
@@ -5,21 +5,29 @@
     <h2 class="">Tickets</h2>
 
   <form id="search">
-    Search <input class="form-control form-control-dark rounded-0 border-1" v-model="searchQuery">
+    Search <input class="form-control form-control-dark rounded-3 border-1" v-model="searchQuery">
   </form>
 
-<table class="table align-items-center mb-0">
+<table class="table align-items-center mb-0" v-if="filteredData.length">
     <thead>
         <tr>
-            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">NAME</th>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">TYPE</th>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Status</th>
-            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">pRIORITY</th>
+            <th @click="sortBy(gridColumns[0])" :class="{   'text-secondary' : sortKey !== gridColumns[0]}"
+                class="text-uppercase   text-xxs font-weight-bolder">
+              {{gridColumns[0]}} <span class="arrow" :class="sortOrders[gridColumns[0]] > 0 ? 'asc' : 'dsc'"></span>
+            </th>
+            <th @click="sortBy(gridColumns[1])" :class="{   'text-secondary' : sortKey !== gridColumns[1]}"
+            class="text-center text-uppercase  text-xxs font-weight-bolder">{{gridColumns[1]}}
+              <span class="arrow" :class="sortOrders[gridColumns[1]] > 0 ? 'asc' : 'dsc'"></span>
+            </th>
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">Status</th>
+            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder ">pRIORITY</th>
             <th class="text-secondary opacity-7"></th>
         </tr>
     </thead>
+
+
     <tbody>
-        <tr v-for="entry in gridData">
+        <tr v-for="entry in filteredData">
             <td>
                 <div class="d-flex px-2 py-1">
                     <div class="d-flex flex-column justify-content-center">
@@ -35,16 +43,16 @@
             </td>
             <td class="align-middle text-center text-sm">
                 <span class="p-1 bg-success text-white bg-opacity-75 rounded-1">{{entry.priority}}</span>
-                
             </td>
-
-            
-            <td class="align-middle"><a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">Details</a></td>
+            <td class="align-middle">
+              <a href="javascript:;" class="text-secondary font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">Details</a>
+            </td>
         </tr>
        
     </tbody>
 </table>
 
+<p v-else>No matches found.</p>
 
 
 
@@ -68,28 +76,34 @@ let user = computed(() => store.state.user);
 
 
 
-
-
-
 const searchQuery = ref('')
 const gridColumns = ['name', 'typ', 'status', 'priority']
-const gridData = [
-  { name: 'Chuck Norris', typ: "BUG" , status: "OPEN" , priority: "LOW" },
-  { name: 'Chuck Norris', typ: "BUG" , status: "OPEN" , priority: "LOW" }
+
+
+const apiData = [
+  { name: 'Ab', typ: "BUG" , status: "OPEN" , priority: "LOW" },
+  { name: 'B', typ: "BUG" , status: "OPEN" , priority: "LOW" }
 ]
 
 
-/*
+
+
+const sortKey = ref('')
+const sortOrders = ref(
+  gridColumns.reduce((o, key) => ((o[key] = 1), o), {})
+)
+
 const filteredData = computed(() => {
-  let { data, filterKey } = props
-  if (filterKey) {
-    filterKey = filterKey.toLowerCase()
+  let data = apiData
+  if (searchQuery) {
+    let searchQuery1 = searchQuery.value.toLowerCase()
     data = data.filter((row) => {
       return Object.keys(row).some((key) => {
-        return String(row[key]).toLowerCase().indexOf(filterKey) > -1
+        return String(row[key]).toLowerCase().indexOf(searchQuery1) > -1
       })
     })
   }
+
   const key = sortKey.value
   if (key) {
     const order = sortOrders.value[key]
@@ -100,13 +114,38 @@ const filteredData = computed(() => {
     })
   }
   return data
-})*/
+})
 
+function sortBy(key) {
+  sortKey.value = key
+  sortOrders.value[key] *= -1
+}
 
 
 
 </script>
     
-<style >
+<style scoped>
+
+.arrow {
+  display: inline-block;
+  vertical-align: middle;
+  width: 0;
+  height: 0;
+  margin-left: 5px;
+  opacity: 0.66;
+}
+
+.arrow.asc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-bottom: 4px solid rgb(0, 0, 0);
+}
+
+.arrow.dsc {
+  border-left: 4px solid transparent;
+  border-right: 4px solid transparent;
+  border-top: 4px solid rgb(0, 0, 0);
+}
 
 </style>
