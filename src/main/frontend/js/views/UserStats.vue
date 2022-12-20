8b39e007fe42844637f73c0bd9e7f8f54592e049
max <max.schoppe@gmx.net>
2022-12-20 14:21:54 +0100

    add graphs to stats site

--- src/main/frontend/js/views/UserStats.vue
+++ src/main/frontend/js/views/UserStats.vue
@@ -1,13 +1,36 @@
 <template >
-    <div class="container mt-5 text-center">
-        <h1>{{ "Hi" + user.id + ", you are logged in as: " + user.userRole  }}</h1>
+    <div class="card mt-3 text-center shadow">
+      <div class="card-header ">
+            <h2>{{ "Hi " + user.id + ", you are logged in as: " + user.userRole  }}</h2>
+      </div>
+        
     
+
+      <div class="card-body">
+
       <div class="row">
-        <div class="col-6">
-          <Doughnut :data="data" :options="options" />
+        <div class="col-md-6 my-2">
+          <div class="card shadow">
+            <div class="card-header">
+              Ticket Priority
+            </div>
+            <div class="card-body">
+              <Doughnut v-if="TicketPriorityData.loaded" :data="TicketPriorityData" :options="options" />
+            </div>
+            
+          </div>
+          
         </div>
-        <div class="col-6">
-          <Doughnut :data="data" :options="options" />
+        <div class=" my-2 col-md-6">
+          <div class="card shadow">
+            <div class="card-header">
+              Ticket Status
+            </div>
+            <div class="card-body">
+              <Doughnut v-if="TicketStatusData.loaded" :data="TicketStatusData" :options="options" />
+            </div>
+          </div>
+          
         </div>
         
       </div>
@@ -15,7 +38,7 @@
     
     </div>
 
-
+</div>
 
     
 
@@ -23,7 +46,7 @@
 </template>
 
 <script setup>
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref , reactive } from 'vue';
 import axios from "axios";
 import { useStore } from 'vuex';
 
@@ -35,15 +58,30 @@ const options = {
   maintainAspectRatio: false
 }
 
-const data = {
-  labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+const TicketStatusData = reactive({
+  loaded: false,
+  labels: null,//['OPEN', 'ASSIGNED', 'MORE INFOS', 'CLOSED'],
+
   datasets: [
     {
-      backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
-      data: [0, 20, 80, 10]
+      backgroundColor: ['#41B883', '#00D8FF', '#DD1B16', '#E46651'],
+      data: null
     }
   ]
-}
+});
+
+
+const TicketPriorityData = reactive( {
+  loaded: false,
+  labels: null,//['LOW', 'MID', 'HIGH', 'NONE'],
+
+  datasets: [
+    {
+      backgroundColor: ['#41B883', '#00D8FF', '#DD1B16', '#E46651'],
+      data: null
+    }
+  ]
+});
 
 
 
@@ -61,6 +99,17 @@ onMounted(async () => {
         const response = await axios.get("/appUsers/stats");
 
         apiData = response.data;
+
+      TicketPriorityData.labels = Object.keys(apiData.totalTicktetsByPriority);
+      TicketPriorityData.datasets[0].data =  Object.values(apiData.totalTicktetsByPriority );
+      //console.log (  Object.values(apiData.totalTicktetsByPriority ) );
+      TicketPriorityData.loaded = true;
+
+
+      TicketStatusData.labels = Object.keys(apiData.totalTicktetsByStatus);
+      TicketStatusData.datasets[0].data =  Object.values(apiData.totalTicktetsByStatus );
+      //console.log (  Object.values(apiData.totalTicktetsByPriority ) );
+      TicketStatusData.loaded = true;
 
 
     } catch (e) {
