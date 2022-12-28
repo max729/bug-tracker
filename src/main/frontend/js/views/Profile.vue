b296b09d693f8af503a4f561b5c2d725a5b7bfae
max <max.schoppe@gmx.net>
2022-12-28 21:04:17 +0100

    frontend add update profil modal

--- src/main/frontend/js/views/Profile.vue
+++ src/main/frontend/js/views/Profile.vue
@@ -36,11 +36,45 @@
     </div>
 
     <div class="card-footer">
-      <button type="button" class="btn btn-primary">Edit</button>
+      <button type="button" @click="openUpdateModal" class="btn btn-primary">Edit</button>
     </div>
 
   </div>
 
+
+  <div  class="modal fade" id="exampleModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">New Ticket</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form>
+          <div class="modal-body">
+            <div class="row">
+              <div class="col-12 my-3 col-sm-6">
+                Fristname : <br>
+                <input v-model="formData.firstName" type="text" class="form-control">
+              </div>
+              <div class="col-12 my-3 col-sm-6">
+                Lastname : <br>
+                <input v-model="formData.lastName" type="text" class="form-control">
+              </div>
+              <div class="col-12 my-3 col-sm-6">
+                Email : <br>
+                <input v-model="formData.email" type="text" class="form-control">
+              </div>
+            </div>
+
+          </div>
+        </form>
+        <div class="modal-footer">
+          <button type="button" id="close" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button @click="submit()" type="button" class="btn btn-primary">Save</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 </template>
@@ -49,14 +83,59 @@
 
 
 <script setup>
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref,reactive } from 'vue';
 import axios from "axios";
 import { useStore } from 'vuex';
+import * as bootstrap from 'bootstrap';
 
 
 const store = useStore();
 let auth = computed(() => store.state.auth);
 let user = computed(() => store.state.user);
+
+const formData = reactive({});
+
+
+
+
+const openUpdateModal = ()=>{
+  Object.assign(formData,store.state.user)
+
+  const modal = bootstrap.Modal.getOrCreateInstance(document.getElementById('exampleModal'));
+
+  modal.show();
+}
+
+const submit = async () => {
+
+try {
+
+  console.log(formData)
+  if (formData.firstName === "" || formData.lastName === "" || formData.email === "" || formData.userRole === "" ) {
+    return;
+  }
+
+  const response = await axios.put("/appUsers/" + formData.id , formData,
+    {
+      'Content-Type': 'application/json',
+      withCredentials: true
+    }
+  )
+
+  if (response.status == 200) {
+
+    const modal = bootstrap.Modal.getInstance(document.getElementById('exampleModal'));
+
+    modal.hide();
+
+
+    await store.dispatch('setUser', formData);
+
+
+  }
+} catch (e) { }
+
+}
     
 
 </script>
